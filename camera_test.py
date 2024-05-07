c71a0fc7f2185bc03710d2a6d62b0954662ad350
gykung <70024742+gykung@users.noreply.github.com>
2024-05-07 22:55:17 +0800

    Update camera_test.py

--- camera_test.py
+++ camera_test.py
@@ -15,7 +15,7 @@ import requests
 # OpenAI API Key
 api_key = st.text_input("API Key")
 
-title = st.text_input("Your prompt:","This is a refinery worker. Are there dangers, work hazards, PPE mistakes or behavioral issues?")
+title = st.text_input("Your prompt:","This is a refinery worker. Are there dangers, work hazards, PPE mistakes or behavioral issues? Are the gloves, helmet, 4-in-1 meter, long sleeve uniform, boots, worn properly?")
 
 
 #img_file_buffer  = st.camera_input("Take a picture")
